36fe474dccc59e5493997488d614d73993c723e7
Krzysztof Wyczlinski <k.wyczlinski.277@studms.ug.edu.pl>
2026-06-05 19:54:33 +0200

    Upgrade Dockerfiles and add reverse proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync("/certs/key.pem"), 
-      cert: fs.readFileSync("/certs/cert.pem"),
-    },
     host: true,
     port: 5173,
     strictPort: true,
-    watch: {
-      usePolling: true,
-    },
-    proxy: {
-      "/api": {
-        target: "https://wiki-api:5000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  }
 });
