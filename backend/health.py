8fae8103be0ffa3f7f4bb947429af87b485705d9
Krzysztof Wyczlinski <k.wyczlinski.277@studms.ug.edu.pl>
2026-07-14 19:07:57 +0200

    Seperate rediness and health ckecks - bug fix

--- backend/health.py
+++ backend/health.py
@@ -6,12 +6,16 @@ health_bp = Blueprint("health", __name__)
 
 @health_bp.route('/health', methods=['GET'])
 def health_check():
+    return jsonify({"status": "healthy"}), 200
+    
+@health_bp.route('/ready', methods=['GET'])
+def readiness_check():
     try:
         with db_conn() as conn:
             with conn.cursor(cursor_factory=RealDictCursor) as cur:
                     cur.execute("SELECT 1;")
 
-        return jsonify({"status": "healthy", "database": "connected"}), 200
+        return jsonify({"status": "ready", "database": "connected"}), 200
     except Exception as err:
         current_app.logger.error(f"Status Unhealthy: {str(err)}")
         return jsonify({"status": "unhealthy", "reason": str(err)}), 500
